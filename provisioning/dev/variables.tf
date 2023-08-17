5858cd8ce5e3f2fbe84e975818f4c395b9948894
Maximilian Temeschinko <maxiteme@peoplesmarkets.com>
2023-08-17 08:33:59 +0200

    fix kics warnings about tf

--- provisioning/dev/variables.tf
+++ provisioning/dev/variables.tf
@@ -1,15 +1,19 @@
 variable "gcloud-project" {
+  type = string
   description = "Google project name"
 }
 
 variable "project-region" {
+  type = string
   description = "Region in which all resources should be provisioned"
 }
 
 variable "key_ring" {
+  type = string
   description = "Cloud KMS key ring name to create"
 }
 
 variable "crypto_key" {
+  type = string
   description = "Crypto key name to create under the key ring"
 }
